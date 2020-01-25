99aacd8f207964e840a91cacb060cdb5359a0077
Qi <liuqi@alphae.cn>
2020-01-24 20:52:11 -0600

    Set view for the placed annotations

--- OnTheMap/Controller/MapViewController.swift
+++ OnTheMap/Controller/MapViewController.swift
@@ -15,10 +15,12 @@ class MapViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    placePinOnMap()
+    
+    mapView.delegate = self
+    placeLocationPinsOnMap()
   }
   
-  func placePinOnMap() {
+  func placeLocationPinsOnMap() {
     
     var locations = [StudentLocation]()
     var annotations = [MKAnnotation]()
@@ -45,6 +47,36 @@ class MapViewController: UIViewController {
         annotations.append(annotation)
       }
       self.mapView.addAnnotations(annotations)
+    }
+  }
+}
+
+extension MapViewController: MKMapViewDelegate {
+  
+  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    
+    let reuseId = "pin"
+    
+    var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+    
+    if pinView == nil {
+      pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+      pinView!.canShowCallout = true
+      pinView!.pinTintColor = .red
+      pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+    } else {
+      pinView!.annotation = annotation
+    }
+    
+  return pinView
+  }
+  
+  func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+    if control == view.rightCalloutAccessoryView {
+      let app = UIApplication.shared
+      if let toOpen = view.annotation?.subtitle {
+        app.open(URL(string: toOpen ?? "")!, options: [:], completionHandler: nil)
+      }
     }
   }
 }
