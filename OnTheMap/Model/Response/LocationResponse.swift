df337487476bc8aa1d9ed0bbd588d2e9a867cd96
Qi <liuqi@alphae.cn>
2020-01-28 02:56:06 -0600

    Refactor struct StudentLocation

--- OnTheMap/Model/Response/LocationResponse.swift
+++ OnTheMap/Model/Response/LocationResponse.swift
@@ -12,16 +12,4 @@ struct LocationResponse: Codable {
   
   let results: [StudentLocation]
 }
-struct StudentLocation: Codable {
-  
-  let createdAt: String
-  let firstName: String
-  let lastName: String
-  let latitude: Double
-  let longitude: Double
-  let mapString: String
-  let mediaURL: String
-  let objectId: String
-  let uniqueKey: String
-  let updatedAt: String
-}
+
